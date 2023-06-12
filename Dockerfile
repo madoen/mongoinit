87ac15f15c4c872dde77d08f647e2f972c30c6e5
Ramdhan Hidayat <ramdhan.h@gmail.com>
2023-06-12 22:29:30 +0700

    Update mongo db hosts in both files

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ FROM mongo:4.4
 # Add your initialization script
 ADD init-replica.js /init-replica.js
 
-CMD ["mongo", "--host", "mongo:giA0CzmQ8PeF26jdquFO@containers-us-west-10.railway.app:6255", "--eval", "load('/init-replica.js')"]
+CMD ["mongo", "--host", "containers-us-west-10.railway.app:6255", "--username", "mongo", "--password", "giA0CzmQ8PeF26jdquFO", "--eval", "load('/init-replica.js')"]
--- init-replica.js
+++ init-replica.js
@@ -1,15 +1,15 @@
-db = (new Mongo("containers-us-west-10.railway.app")).getDB("appsmith");
+db = (new Mongo("containers-us-west-10.railway.app:6255")).getDB("test");
 config = {
-"_id" : "replicaSet",
+"_id" : "my-mongo-set",
 "members" : [
   {
     "_id" : 0,
-    "host" : "mongo:giA0CzmQ8PeF26jdquFO@containers-us-west-10.railway.app:6255"
+    "host" : "containers-us-west-10.railway.app:6255"
   },
   {
     "_id" : 1,
-    "host" : "mongo:kTzJoWPUv3BfIr9MIkO5@containers-us-west-84.railway.app:7986"
-  },
+    "host" : "containers-us-west-84.railway.app:7986"
+  }
 ]
 };
 rs.initiate(config);
